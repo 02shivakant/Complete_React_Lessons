c11e832de64c653fedd753e5cb3ed7ecb0c55fae
dearravan02 <sanjaypandey2334@gmail.com>
2026-02-01 00:30:31 +0530

    contional rendering complated

---
+++ Level6/conditional rendering/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Logout from './components/Logout'
+import Login from './components/Login'
+
+function App() {
+  const [isLogin, setisLogin] = useState(false)
+
+  
+    
+      if(isLogin){
+        return (
+          <Logout/>
+        )
+    }
+      else{
+        return(
+          <Login/>
+        )
+      }
+  }
+  
+export default App
